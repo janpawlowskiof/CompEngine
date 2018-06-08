d0e47f4197e7d715ea7f473258978c26dffb8fdb
blondyn526j <blondyn526j@gmail.com>
2018-06-08 10:36:38 +0200

    Added calculation for approximated collision point

--- CompEng/myTestScipt.h
+++ CompEng/myTestScipt.h
@@ -7,40 +7,70 @@
 
 extern RendererMaster* rendererMaster;
 
+struct SupportPoint
+{
+	glm::vec3 point;	//point on minkowski diffrence
+	glm::vec3 supportA;	//point of SuppportA - used to get collision point
+	glm::vec3 supportB;
+
+	SupportPoint(glm::vec3 p, glm::vec3 sA, glm::vec3 sB)
+	{
+		point = p;
+		supportA = sA;
+		supportB = sB;
+	}
+	SupportPoint() {};
+};
+
 class Simplex
 {
 public:
 	int count = 0;
-	glm::vec3 P[4];
+	SupportPoint supports[4];
+	glm::vec3 normal;	//normal always pointing away from the origin, regardless of the winding
 
-	void Add(glm::vec3 A)
+	void Add(SupportPoint A)
 	{
-		P[count] = A;
+		supports[count] = A;
 		count++;
+
+		if (count == 3)
+			CalculateNormal();
 	}
 
-	Simplex(glm::vec3 A)
+	Simplex(SupportPoint A)
 	{
-		P[0] = A;
+		supports[0] = A;
 		count = 1;
 	}
 
-	Simplex(glm::vec3 A, glm::vec3 B)
+	Simplex(SupportPoint A, SupportPoint B)
 	{
-		P[0] = B;
-		P[1] = A;
+		supports[0] = B;
+		supports[1] = A;
 		count = 2;
 	}
 
-	Simplex(glm::vec3 A, glm::vec3 B, glm::vec3 C)
+	Simplex(SupportPoint A, SupportPoint B, SupportPoint C)
 	{
-		P[0] = C;
-		P[1] = B;
-		P[2] = A;
+		supports[0] = C;
+		supports[1] = B;
+		supports[2] = A;
 		count = 3;
+		CalculateNormal();
 	}
 
 	Simplex() {};
+
+private:
+	void CalculateNormal()
+	{
+		normal = glm::normalize(glm::cross(supports[1].point - supports[0].point, supports[2].point - supports[0].point));	//normal
+		if (glm::dot(normal, supports[1].point) < 0)	//normal pointing inwards (we want it outwards)
+			normal = -normal;	//we now have normal
+		if (normal.x != normal.x)	//NaN error, objects probably touching
+			normal = glm::vec3(1, 1, 1);
+	}
 
 	//das broken :c
 	void DebugDraw(glm::vec3 color)
@@ -52,37 +82,32 @@ public:
 		MeshComponent* mesh = (MeshComponent*)cube->GetComponent("Mesh");
 		mesh->diffuseColor = color;
 		mesh->specularColor = color;
-		mesh->vertices[0].position = P[0];
-		mesh->vertices[1].position = P[1];
-		mesh->vertices[2].position = P[2];
-		glm::vec3 n = glm::normalize(glm::cross(P[2] - P[0], P[1] - P[0]));
-		mesh->vertices[0].normal = P[0];
-		mesh->vertices[1].normal = P[1];
-		mesh->vertices[2].normal = P[2];
+		mesh->vertices[0].position = supports[0].point;
+		mesh->vertices[1].position = supports[1].point;
+		mesh->vertices[2].position = supports[2].point;
+		glm::vec3 n = glm::normalize(glm::cross(supports[2].point - supports[0].point, supports[1].point - supports[0].point));
+		mesh->vertices[0].normal = supports[0].point;
+		mesh->vertices[1].normal = supports[1].point;
+		mesh->vertices[2].normal = supports[2].point;
 
 		mesh->Initialize();
 
 		rendererMaster->currentShader->setMat4("modelMatrix", model);
 		mesh->Draw(rendererMaster->currentShader);
-		//Sleep(100);
-		//glBegin(GL_TRIANGLES);
-		/*glVertex3f(P[0].x, P[0].y, P[0].z);
-		glVertex3f(P[1].x, P[1].y, P[1].z);
-		glVertex3f(P[2].x, P[2].y, P[2].z);
-
-		glEnd();*/
 	}
 };
 
 class CollsionData
 {
 public:
+	glm::vec3 contactPoint;
 	glm::vec3 normal;
 	float penetration;
 
 	CollsionData() {};
-	CollsionData(glm::vec3 normal, float penetration)
+	CollsionData(glm::vec3 contactPoint, glm::vec3 normal, float penetration)
 	{
+		this->contactPoint = contactPoint;
 		this->normal = normal;
 		this->penetration = penetration;
 	}
@@ -93,7 +118,7 @@ class MyTestScript : public UserScriptComponent
 public:
 	float value = 0;
 
-	BaseObject* cubeA, *cubeB, *cubeC;
+	BaseObject *cubeA, *cubeB, *cubeC, *sphereA;
 	MeshComponent* meshA, *meshB, *meshC;
 
 	glm::vec4 boxPoints[8] = {
@@ -254,90 +279,24 @@ public:
 		}
 		return furthestPoint;
 	}
-	glm::vec3 Support(glm::vec3 D)
+	SupportPoint Support(glm::vec3 D)
 	{
-		//return SupportTEST(D);
-		return SupportA(D) - SupportB(-D);
+		auto supportA = SupportA(D);
+		auto supportB = SupportB(-D);
+		return SupportPoint(supportA - supportB, supportA, supportB);
 	}
-	/*
-	glm::vec3 ClosestPointOnLine(glm::vec3 P, glm::vec3 A, glm::vec3 B)
-	{
-		float t = glm::dot(P - A, glm::normalize(B - A));
-		if (t < 0)
-			t = 0;
-		else if (t > 1)
-			t = 1;
-		return A + t * glm::normalize(B - A);
-	}
-	/*
-	glm::vec3 ClosestPointOn2DTriangle(glm::vec3 P, Simplex triangle)
-	{
-		glm::vec3 A = triangle.P[0];
-		glm::vec3 B = triangle.P[1];
-		glm::vec3 C = triangle.P[2];
-
-		float area = glm::length(glm::cross(B - A, C - A));
-		float pointArea = glm::length(glm::cross(C - A, P - A)) +	//unfortunate name
-			glm::length(glm::cross(B - C, P - C)) +
-			glm::length(glm::cross(A - B, P - B));
-
-
-		if (area == pointArea)	//is point inside the triangle?
-		{
-			//inside the triangle
-			return P;
-			//return (B - A) * w1 + (C - A) * w2;	//im tired
-		}
-		else
-		{
-			//outside the triangle
-			//AB
-			glm::vec3 closestPoint = ClosestPointOnLine(P, A, B);
-			float minDistance = glm::length(P - closestPoint);
-			//BC
-			glm::vec3 closestOnCurrent = ClosestPointOnLine(P, B, C);
-			float distanceOnCurrent = glm::length(P - closestOnCurrent);
-			if (distanceOnCurrent < minDistance)
-			{
-				minDistance = distanceOnCurrent;
-				closestPoint = closestOnCurrent;
-			}
-			//CA
-			closestOnCurrent = ClosestPointOnLine(P, C, A);
-			distanceOnCurrent = glm::length(P - closestOnCurrent);
-			if (distanceOnCurrent < minDistance)
-			{
-				minDistance = distanceOnCurrent;
-				closestPoint = closestOnCurrent;
-			}
-			return closestPoint;
-		}
-	}
-	/*
-	glm::vec3 ProjectPointOnTrianglePlane(glm::vec3 P, Simplex triangle)
-	{
-		glm::vec3 N = glm::normalize(glm::cross(triangle.P[1] - triangle.P[0], triangle.P[2] - triangle.P[0]));	//normal
-		return P - N * glm::dot(P - triangle.P[0], N);
-	}*/
 
 	float OriginTringleDistance(Simplex triangle)
 	{
-		glm::vec3 N = glm::normalize(glm::cross(triangle.P[1] - triangle.P[0], triangle.P[2] - triangle.P[0]));	//normal
-		return abs(glm::dot(N, triangle.P[0]));
+		glm::vec3 N = glm::normalize(glm::cross(triangle.supports[1].point - triangle.supports[0].point, triangle.supports[2].point - triangle.supports[0].point));	//normal
+		return abs(glm::dot(N, triangle.supports[0].point));
 	}
-	/*
-	float PointTriangleDistace(glm::vec3 P, Simplex triangle)
-	{
-		glm::vec3 projectedPoint = ProjectPointOnTrianglePlane(P, triangle);
-		glm::vec3 closestPointOnTriangle = ClosestPointOn2DTriangle(projectedPoint, triangle);
-		return glm::length(P - closestPointOnTriangle);
-	}
-	*/
+
 	void AddEdge(Simplex edge, std::vector<Simplex>& edges)
 	{
 		for (int e = 0; e < edges.size(); ++e)
 		{
-			if ((edges[e].P[0] == edge.P[0] && edges[e].P[1] == edge.P[1]) || (edges[e].P[0] == edge.P[1] && edges[e].P[1] == edge.P[0]))
+			if ((edges[e].supports[0].point == edge.supports[0].point && edges[e].supports[1].point == edge.supports[1].point) || (edges[e].supports[0].point == edge.supports[1].point && edges[e].supports[1].point == edge.supports[0].point))
 			{
 				edges.erase(edges.begin() + e);
 				return;
@@ -346,13 +305,28 @@ public:
 		edges.push_back(edge);
 	}
 
+	//Christer Ericson's Real-Time Collision Detection
+	void Barycentric(glm::vec3 p, glm::vec3 a, glm::vec3 b, glm::vec3 c, float &u, float &v, float &w)
+	{
+		glm::vec3 v0 = b - a, v1 = c - a, v2 = p - a;
+		float d00 = glm::dot(v0, v0);
+		float d01 = glm::dot(v0, v1);
+		float d11 = glm::dot(v1, v1);
+		float d20 = glm::dot(v2, v0);
+		float d21 = glm::dot(v2, v1);
+		float denom = d00 * d11 - d01 * d01;
+		v = (d11 * d20 - d01 * d21) / denom;
+		w = (d00 * d21 - d01 * d20) / denom;
+		u = 1.0f - v - w;
+	}
+
 	CollsionData BuildPolytope(Simplex base)
 	{
 		std::vector<Simplex> faces;
-		faces.push_back(Simplex(base.P[0], base.P[1], base.P[2]));
-		faces.push_back(Simplex(base.P[0], base.P[2], base.P[3]));
-		faces.push_back(Simplex(base.P[1], base.P[2], base.P[3]));
-		faces.push_back(Simplex(base.P[0], base.P[1], base.P[3]));
+		faces.push_back(Simplex(base.supports[0], base.supports[1], base.supports[2]));
+		faces.push_back(Simplex(base.supports[0], base.supports[2], base.supports[3]));
+		faces.push_back(Simplex(base.supports[1], base.supports[2], base.supports[3]));
+		faces.push_back(Simplex(base.supports[0], base.supports[1], base.supports[3]));
 
 		int counter = 0;
 		while (true)
@@ -375,20 +349,30 @@ public:
 					closestFaceIndex = i;
 				}
 			}
-			//we now have closest face and its index
-			glm::vec3 N = glm::normalize(glm::cross(faces[closestFaceIndex].P[1] - faces[closestFaceIndex].P[0], faces[closestFaceIndex].P[2] - faces[closestFaceIndex].P[0]));	//normal
-			if (glm::dot(N, faces[closestFaceIndex].P[1]) < 0)	//normal pointing inwards (we want it outwards)
-				N = -N;	//we now have normal
-			if (N != N)
-				N = glm::vec3(1, 1, 1);
 
-			glm::vec3 A = Support(N);
-			float distanceOA = abs(glm::dot(A, N));	//distance to our new point
+			//N = faces[closestFaceIndex].normal
+			SupportPoint A = Support(faces[closestFaceIndex].normal);
+			float distanceOA = abs(glm::dot(A.point, faces[closestFaceIndex].normal));	//distance to our new point
 
 			if (distanceOA - closestDistance < 0.0001)	//is our new point far enough?
 			{
-				//it's to close - terminate
-				return CollsionData(N, closestDistance);
+				//it's to close - we found our (almost?) closest triangle
+				float barycentricU, barycentricV, barycentricW;
+
+				Barycentric(faces[closestFaceIndex].normal * closestDistance,
+					faces[closestFaceIndex].supports[0].point,
+					faces[closestFaceIndex].supports[1].point,
+					faces[closestFaceIndex].supports[2].point,
+					barycentricU, barycentricV, barycentricW);
+				glm::vec3 collisionPointA = faces[closestFaceIndex].supports[0].supportA * barycentricU +
+					faces[closestFaceIndex].supports[1].supportA * barycentricV +
+					faces[closestFaceIndex].supports[2].supportA * barycentricW;
+
+				glm::vec3 collisionPointB = faces[closestFaceIndex].supports[0].supportB * barycentricU +
+					faces[closestFaceIndex].supports[1].supportB * barycentricV +
+					faces[closestFaceIndex].supports[2].supportB * barycentricW;
+
+				return CollsionData((collisionPointA + collisionPointB)/2.0f, -faces[closestFaceIndex].normal, closestDistance);
 			}
 			else
 			{
@@ -441,23 +425,16 @@ public:
 				for (int i = 0; i < faces.size(); ++i)
 				{
 					if (faces[i].count == 2)
-						continue;
+						continue;	//just a failsafe
 
-					glm::vec3 currentNormal = glm::normalize(glm::cross(faces[i].P[1] - faces[i].P[0], faces[i].P[2] - faces[i].P[0]));
-					if (glm::dot(currentNormal, faces[i].P[0]) < 0)	//normal pointing inwards (we want it outwards)
-						currentNormal = -currentNormal;	//we now have normal
-
-					if (glm::dot(A - faces[i].P[0], currentNormal) > 0)	//can face be seen?
+					if (glm::dot(A.point - faces[i].supports[0].point, faces[i].normal) > 0)	//can face be seen?
 					{
 						//yes
 						//add egde to edge if such edgde doesnt exist
 						//if said edge already exists than delete both instances
-						//Simplex edge;
-						AddEdge(Simplex(faces[i].P[0], faces[i].P[1]), edges);
-
-						AddEdge(Simplex(faces[i].P[1], faces[i].P[2]), edges);
-
-						AddEdge(Simplex(faces[i].P[0], faces[i].P[2]), edges);
+						AddEdge(Simplex(faces[i].supports[0], faces[i].supports[1]), edges);
+						AddEdge(Simplex(faces[i].supports[1], faces[i].supports[2]), edges);
+						AddEdge(Simplex(faces[i].supports[0], faces[i].supports[2]), edges);
 
 						faces.erase(faces.begin() + i);
 						i--;
@@ -466,17 +443,9 @@ public:
 
 				for (int i = 0; i < edges.size(); ++i)
 				{
-					faces.push_back(Simplex(edges[i].P[0], edges[i].P[1], A));
+					faces.push_back(Simplex(edges[i].supports[0], edges[i].supports[1], A));
 				}
 				edges.clear();
-
-				//add newly created faces
-				//faces.push_back(Simplex(faces[closestFaceIndex].P[0], faces[closestFaceIndex].P[1], A));
-				//faces.push_back(Simplex(faces[closestFaceIndex].P[1], faces[closestFaceIndex].P[2], A));
-				//faces.push_back(Simplex(faces[closestFaceIndex].P[0], faces[closestFaceIndex].P[2], A));
-
-				//delete old one
-				//faces.erase(faces.begin() + closestFaceIndex);
 			}
 		}
 		return CollsionData();
@@ -486,30 +455,31 @@ public:
 	{
 		if (simplex.count == 2)
 		{
-			glm::vec3 a = simplex.P[1];
-			glm::vec3 b = simplex.P[0];
+			SupportPoint a = simplex.supports[1];
+			SupportPoint b = simplex.supports[0];
 
-			glm::vec3 ab = b - a;
-			glm::vec3 ao = -a;
+			glm::vec3 ab = b.point - a.point;
+			glm::vec3 ao = -a.point;
 
 			simplex = Simplex(a, b);	//????
 			D = glm::cross(glm::cross(ab, ao), ab);
 
 			if (D == glm::vec3(0))		//s.o.s.
-				D = glm::cross(ab, a + glm::vec3(1));
+				D = glm::cross(ab, a.point + glm::vec3(1));
 
 			return false;
 		}
 		else if (simplex.count == 3)
 		{
-			glm::vec3 a = simplex.P[2];
-			glm::vec3 b = simplex.P[1];
-			glm::vec3 c = simplex.P[0];
+			SupportPoint a = simplex.supports[2];
+			SupportPoint b = simplex.supports[1];
+			SupportPoint c = simplex.supports[0];
 
-			glm::vec3 ao = -a;
+			glm::vec3 ao = -a.point;
 
-			glm::vec3 ab = b - a;
-			glm::vec3 ac = c - a;
+			glm::vec3 ab = b.point - a.point;
+			glm::vec3 ac = c.point - a.point;
+			
 			glm::vec3 abc = glm::cross(ab, ac);	//normal
 
 			if (glm::dot(glm::cross(ab, abc), ao) > 0)
@@ -536,15 +506,15 @@ public:
 		}
 		else if (simplex.count == 4)
 		{
-			glm::vec3 a = simplex.P[3];
-			glm::vec3 b = simplex.P[2];
-			glm::vec3 c = simplex.P[1];
-			glm::vec3 d = simplex.P[0];
-			glm::vec3 ao = -a;
+			SupportPoint a = simplex.supports[3];
+			SupportPoint b = simplex.supports[2];
+			SupportPoint c = simplex.supports[1];
+			SupportPoint d = simplex.supports[0];
+			glm::vec3 ao = -a.point;
 
-			glm::vec3 ab = b - a;
-			glm::vec3 ac = c - a;
-			glm::vec3 ad = d - a;
+			glm::vec3 ab = b.point - a.point;
+			glm::vec3 ac = c.point - a.point;
+			glm::vec3 ad = d.point - a.point;
 
 			if (glm::dot(glm::cross(ab, ac), ao) > 0)
 			{
@@ -560,17 +530,17 @@ public:
 			}
 			else
 			{
-				///
 				collisionData = BuildPolytope(simplex);
 				return true;	//NICEEEEEEEE
 			}
 
 			//What to do with our selected face
-			a = simplex.P[2];
-			b = simplex.P[1];
-			c = simplex.P[0];
-			ab = b - a;
-			ac = c - a;
+			a = simplex.supports[2];
+			b = simplex.supports[1];
+			c = simplex.supports[0];
+			ab = b.point - a.point;
+			ac = c.point - a.point;
+
 			glm::vec3 abc = glm::cross(ab, ac);
 
 			if (glm::dot(glm::cross(ab, abc), ao) > 0)
@@ -594,17 +564,16 @@ public:
 
 	bool BoxesCollideGJK(CollsionData& collisionData)
 	{
-		glm::vec3 S = Support(glm::vec3(0, 1, 0));	//random dir
+		SupportPoint S = Support(glm::vec3(0, 1, 0));	//random dir
 		Simplex simplex(S);
-		glm::vec3 D = -S;
+		glm::vec3 D = -S.point;
 
 		int counter = 0;
 		while (counter < 64)	//max iterations
 		{
-			glm::vec3 A = Support(D);
-			if (glm::dot(A, D) < 0)
+			SupportPoint A = Support(D);
+			if (glm::dot(A.point, D) < 0)
 			{
-				//std::cout << counter << std::endl;
 				return false;
 			}
 			simplex.Add(A);
@@ -624,24 +593,20 @@ public:
 		cubeA = BaseObject::Find("Cube1");
 		cubeB = BaseObject::Find("Cube2");
 		cubeC = BaseObject::Find("Cube3");
+		sphereA = BaseObject::Find("Sphere1");
 
 		TransformComponent* transformA = (TransformComponent*)cubeA->GetComponent("Transform");
-		//transformA->Rotate(glm::angleAxis(1.01f, glm::vec3(0, 0, 1)));
-		transformA->position = glm::vec3(-1.05f, 1.0f, 0);
+		transformA->position = glm::vec3(-2.35f, 1.0f, 0);
 		TransformComponent* transformB = (TransformComponent*)cubeB->GetComponent("Transform");
 		transformB->position = glm::vec3(0.0f, 1.0f, 0.0f);
 		TransformComponent* transformC = (TransformComponent*)cubeC->GetComponent("Transform");
 		transformC->position = glm::vec3(0.0f, 4.0f, 0.0f);
 
+		transformA->Rotate(glm::angleAxis(0.4f, glm::vec3(0, 0, 1)));
+		//transformB->Rotate(glm::angleAxis(0.7f * interval, glm::vec3(-0.3, 0.1, 0.3)));
+
 		meshA = (MeshComponent*)cubeA->GetComponent("Mesh");
 		meshB = (MeshComponent*)cubeB->GetComponent("Mesh");
-
-		//TESTING
-		/*Simplex s(glm::vec3(-1, 0, 0), glm::vec3(1, 0, 0), glm::vec3(0, 1, 0));
-		//glm::vec3 p = ClosestPointOn2DTriangle(glm::vec3(1, 1, 0), s);
-		//glm::vec3 p = ClosestPointOnLine(glm::vec3(1, 1, 0), glm::vec3(1, 0, 0), glm::vec3(0, 1, 0));
-		auto p = SupportTEST(glm::vec3(0, 1, 0));
-		std::cout << "Ans: " << p.x << " - " << p.y << " - " << p.z << std::endl;*/
 	}
 
 	void Update()
@@ -652,11 +617,13 @@ public:
 		TransformComponent* transformA = (TransformComponent*)cubeA->GetComponent("Transform");
 		TransformComponent* transformB = (TransformComponent*)cubeB->GetComponent("Transform");
 		TransformComponent* transformC = (TransformComponent*)cubeC->GetComponent("Transform");
+		TransformComponent* transformSphere = (TransformComponent*)sphereA->GetComponent("Transform");
 
 		transformA->position += 0.1f * glm::vec3(glfwGetKey(rendererMaster->window, GLFW_KEY_L) - glfwGetKey(rendererMaster->window, GLFW_KEY_J),
-			0,
-			glfwGetKey(rendererMaster->window, GLFW_KEY_I) - glfwGetKey(rendererMaster->window, GLFW_KEY_K));
-		//transformA->Rotate(glm::angleAxis(interval, glm::vec3(0, 0, 1)));
+			glfwGetKey(rendererMaster->window, GLFW_KEY_U) - glfwGetKey(rendererMaster->window, GLFW_KEY_O),
+			glfwGetKey(rendererMaster->window, GLFW_KEY_K) - glfwGetKey(rendererMaster->window, GLFW_KEY_I));
+		//transformA->Rotate(glm::angleAxis(1.4f * interval, glm::vec3(0, 0, 1)));
+		//transformB->Rotate(glm::angleAxis(0.7f * interval, glm::vec3(-0.3, 0.1, 0.3)));
 
 		glm::mat4 modelA = transformA->GetModelMatrix();
 		glm::mat4 modelB = transformB->GetModelMatrix();
@@ -670,10 +637,12 @@ public:
 		if (BoxesCollideGJK(collisionData))
 		{
 			meshA->diffuseColor = glm::vec3(1, 0, 0);
+			transformSphere->position = collisionData.contactPoint;
 		}
 		else
 		{
 			meshA->diffuseColor = glm::vec3(0, 1, 0);
+			transformSphere->position = glm::vec3(20, 0, 0);
 		}
 		transformC->rotation = glm::lookAt(glm::vec3(0), collisionData.normal, glm::vec3(0, 1, 0));
 	}
